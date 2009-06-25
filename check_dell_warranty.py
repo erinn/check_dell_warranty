2669f7ee1fae1977e9dc246ac408f06dea81a3cd
Erinn Looney-Triggs <looneytr@fate.colorado.edu>
2009-06-25 12:19:55 -0600

    Made it accept multiple serial numbers. urllib is still sync so it will be slow.

--- check_dell_warranty.py
+++ check_dell_warranty.py
@@ -3,16 +3,23 @@
 #=============================================================================
 # Nagios plug-in to pull the Dell service tag and check it 
 # against Dell's web site to see how many days remain. By default it 
-# issues a warning when there is less than thirty days remaining and critical when 
-# there is less than ten days remaining. These values can be adjusted using
-# the command line, see --help.                                                 
-# Version: 1.4                                                                
+# issues a warning when there is less than thirty days remaining and critical 
+# when there is less than ten days remaining. These values can be adjusted 
+# using the command line, see --help.                                                 
+# Version: 1.5                                                                
 # Created: 2009-02-12                                                         
 # Author: Erinn Looney-Triggs                                                 
-# Revised: 2009-06-05                                                                
+# Revised: 2009-06-25                                                                
 # Revised by: Erinn Looney-Triggs, Justin Ellison                                                                
 # Revision history:
 #
+# 2009-06-25 1.5: Changed optparse to handle multiple serial numbers. Changed
+# the rest of the program to be able to handle multiple serial numbers. Added
+# a de-duper for serial numbers just in case you get two of the same from
+# the command line or as is the case with Dell blades, two of the same
+# from omreport. So this ought to handle blades, though I don't have
+# any to test against. 
+# 
 # 2009-06-05 1.4 Changed optparse to display %default in help output. Pretty
 # up the help output with <ARG> instead of variable names. Add description
 # top optparse. Will now use prefer omreport to dmidecode for systems
@@ -92,6 +99,8 @@ def extract_serial_number():
     omreport = which('omreport')
     dmidecode = which('dmidecode')
     
+    serial_numbers = []
+    
     if omreport:
         import re
         try:
@@ -105,7 +114,7 @@ def extract_serial_number():
         text = p.stdout.read()
         pattern = '''<ServiceTag>(\S+)</ServiceTag>'''
         regex = re.compile(pattern, re.X)
-        serial_number = "".join(regex.findall(text))
+        serial_numbers = regex.findall(text)
         
     elif dmidecode: 
         #Gather the information from dmidecode
@@ -117,22 +126,21 @@ def extract_serial_number():
         except OSError:
             print 'Error:', sys.exc_value, 'exiting!'
             sys.exit(WARNING)
-        serial_number = p.stdout.read().strip()
+        serial_numbers.append(p.stdout.read().strip())
         
     else:
-        print 'Neither omreport or dmidecode are available in $PATH, aborting!'
-        sys.exit(WARNING)
-   
-    #Basic check of the serial number, can they be longer? Maybe.
-    if len( serial_number ) != 7:
-        print 'Invalid serial number: %s exiting!' % (serial_number)
+        print 'Neither omreport nor dmidecode are available in $PATH, aborting!'
         sys.exit(WARNING)
     
-    return serial_number
 
-def get_warranty(serial_number):
+    
+    return serial_numbers
+
+def get_warranty(serial_numbers):
     import re
     import urllib2
+    
+    result_list = [] #List of results
     
     #The URL to Dell's site
     dell_url='http://support.dell.com/support/topics/global.aspx/support/my_systems_info/details?c=us&l=en&s=gen&ServiceTag='
@@ -146,70 +154,92 @@ def get_warranty(serial_number):
                 <                            #Match <
                 """
     
-    #Build the full URL
-    full_url = dell_url + serial_number
+    # Remove duplicates:
+    if len( serial_numbers ) > 1:
+        serial_numbers = dict.keys(dict.fromkeys(serial_numbers))
     
-    #Try to open the page, exit on failure
-    try:
-        response = urllib2.urlopen(full_url)
-    except URLError:
-        print 'Unable to open URL: %s exiting!' % (full_url)
-        sys.exit(UNKNOWN)
+    #Basic check of the serial number, can they be longer? Maybe.
+    for number in serial_numbers:
+        if len( number ) != 7:
+            print 'Invalid serial number: %s exiting!' % (number)
+            sys.exit(WARNING)
     
-    #Build our regex
-    regex = re.compile(pattern, re.X)
+    #TODO: Make this async in the future
+    for serial_number in serial_numbers:   
+        #Build the full URL
+        full_url = dell_url + serial_number
+        
+        #Try to open the page, exit on failure
+        try:
+            response = urllib2.urlopen(full_url)
+        except URLError:
+            print 'Unable to open URL: %s exiting!' % (full_url)
+            sys.exit(UNKNOWN)
+        
+        #Build our regex
+        regex = re.compile(pattern, re.X)
+        
+        #Gather the results returns a list of tuples
+        result_list.append((regex.findall(response.read()), serial_number))
     
-    #Gather the results returns a list of tuples
-    result = (regex.findall(response.read()), serial_number)
-    
-    return result
+    return result_list
 
-def parse_exit(result):
+def parse_exit(result_list):
     import datetime
     
-    days = 0
-    dates = []
+    critical = 0
+    warning = 0
     
-    serial_number = result[1]
-    
-    if len(result[0]) == 0:
-        print "Dell's database appears to be down."
-        sys.exit(WARNING)
-    
-    for match in result[0]:
-        start_date, end_date, days_left = match
+    for result in result_list:
+        days = 0
+        dates = []
         
-        #This makes this plugin limited to North American style dates but
-        #as long as the service tag is run through the dell.com website
-        #it does not matter. (I think)
-        for date in start_date, end_date:
-            month, day, year = date.split('/')
-            dates.append(datetime.date(int(year), int(month), int(day)))
+        serial_number = result[1]
         
-        days += int(days_left)
-    
-    dates.sort()
-    
-    start_date = dates[0]
-    end_date = dates[-1]
-    days_left = days
+        if len(result[0]) == 0:
+            print "Dell's database appears to be down."
+            sys.exit(WARNING)
         
-    if days_left < options.critical_days:
-        print 'CRITICAL: Service Tag: %s Warranty start: %s End: %s Days left: %s' \
-        % (serial_number, start_date, end_date, days_left)
+        for match in result[0]:
+            start_date, end_date, days_left = match
+            
+            #This makes this plugin limited to North American style dates but
+            #as long as the service tag is run through the dell.com website
+            #it does not matter. (I think)
+            for date in start_date, end_date:
+                month, day, year = date.split('/')
+                dates.append(datetime.date(int(year), int(month), int(day)))
+            
+            days += int(days_left)
+        
+        dates.sort()
+        
+        start_date = dates[0]
+        end_date = dates[-1]
+        days_left = days
+        
+        if days_left < options.critical_days:
+            state ='CRITICAL'
+            critical+= 1
+            
+        elif days_left < options.warning_days:
+            state = 'WARNING'
+            warning+= 1
+            
+        else:
+            state = 'OK'
+            
+        print '| %s: Service Tag: %s Warranty start: %s End: %s Days left: %d |' \
+            % (state, serial_number, start_date, end_date, days_left),
+        
+    if critical:
         sys.exit(CRITICAL)
-        
-    elif days_left < options.warning_days:
-        print 'WARNING: Service Tag: %s Warranty start: %s End: %s Days left: %s' \
-        % (serial_number, start_date, end_date, days_left)
+    elif warning:
         sys.exit(WARNING)
-        
     else:
-        print 'OK: Service Tag: %s Warranty start: %s End: %s Days left: %s' \
-        % (serial_number, start_date, end_date, days_left)
         sys.exit(OK)
-        
-    return None 
+    
+    return None #Should never get here
 
 def sigalarm_handler(signum, frame):
     print '%s timed out after %d seconds' % (sys.argv[0], options.timeout)
@@ -228,13 +258,14 @@ thirty days remaining and critical when there is less than ten days \
 remaining. These values can be adjusted using the command line, see --help.
     ''',
                                    prog="check_dell_warranty",
-                                   version="%prog Version: 1.4")
+                                   version="%prog Version: 1.5")
     parser.add_option('-c', '--critical', dest='critical_days', default=10,
                      help='Number of days under which to return critical \
                      (Default: %default)', type='int', metavar='<ARG>')
     parser.add_option('-s', '--serial-number', dest='serial_number', 
-                      default='', help='Dell Service Tag of system \
-                      (Default: auto-detected)', type='string', metavar='<ARG>')
+                       help='Dell Service Tag of system, to enter more than \
+                      one use multiple flags (Default: auto-detected)',  
+                      action='append', metavar='<ARG>')
     parser.add_option('-t', '--timeout', dest='timeout', default=10,
                       help='Set the timeout for the program to run \
                       (Default: %default seconds)', type='int', metavar='<ARG>')
@@ -248,11 +279,11 @@ remaining. These values can be adjusted using the command line, see --help.
     signal.alarm(options.timeout)
     
     if options.serial_number:
-        serial_number = options.serial_number
+        serial_numbers = options.serial_number
     else:
-        serial_number = extract_serial_number()
+        serial_numbers = extract_serial_number()
     
-    result = get_warranty(serial_number)
+    result = get_warranty(serial_numbers)
     
     signal.alarm(0)
     
